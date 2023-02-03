ee0c6b0f2f9a93bd353c71b5c7a0a4de4423a8d1
Paulo Castelo <paulocastelo_fl@live.com>
2023-02-03 01:11:58 -0400

    menu-lateral-cadastros

--- src/app/configuracoes/empresa/empresa.component.ts
+++ src/app/configuracoes/empresa/empresa.component.ts
@@ -2,13 +2,31 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-empresa',
-  templateUrl: './empresa.component.html'
+  templateUrl: './empresa.component.html',
+  styleUrls: ['./empresa.component.scss']
 })
 export class EmpresaComponent implements OnInit {
+
+  public isTabSelect: string = "Basico";
+
+  public listTabs = [
+    "Basico",
+    "Usuários",
+    "Importação",
+    "Agenciamento de Carga",
+    "Documentos",
+    "Credenciais",
+    "Financeiro",
+    "Relatórios"
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  isSelectTab(tab){
+    this.isTabSelect = tab;
   }
 
 }
