aebd29924af55dd341103cc35d6c555e7aa33d8d
edilosousa <edilo13@gmail.com>
2023-03-06 17:13:13 -0400

    Atualização no modulo de Fornecedor.

--- src/app/configuracoes/cards-forms/importacao/forms/fornecedores/fornecedores.component.ts
+++ src/app/configuracoes/cards-forms/importacao/forms/fornecedores/fornecedores.component.ts
@@ -21,19 +21,32 @@ declare interface DataTable {
 })
 export class FornecedoresComponent implements OnInit {
 
-  public dataTable: DataTable;
+  // public dataTable: DataTable;
+  // public dtOptions: any = {}
+  // public table;
+  // public isLoad: boolean = false;
+  // public subscription: Subscription;
+  // public closeResult = '';
+  // public modalRef?: BsModalRef;
+  // public form: FormGroup;
+  // public submitted = false;
+  // public isLoadSave: boolean = false;
+  // public paises: IPais[];
+  // public userIdupdate: number = 0
 
-  public dtOptions: any = {}
-  public table;
-  public isLoad: boolean = false;
-  public subscription: Subscription;
-  public closeResult = '';
   public modalRef?: BsModalRef;
   public form: FormGroup;
+  public isLoadSave = false;
+  @Input() empresa: Empresa;
   public submitted = false;
-  public isLoadSave: boolean = false;
+  isLoad: boolean = false;
+  public titleModal = "Novo";
   public paises: IPais[];
-  public userIdupdate: number = 0
+  public fornecedorList: IFornecedor[] = [];
+  public listFornecedorListDynamic: IFornecedor[] = [];
+  idUpdate: number = 0;
+
+  public p = 1;
 
   constructor(
     private service: FornecedorService,
@@ -41,66 +54,132 @@ export class FornecedoresComponent implements OnInit {
     private formBuilder: FormBuilder,
     private notifyService: NotifyService,
     private paisService: PaisService
-  ) { }
+  ) {
+
+    this.form = this.formBuilder.group({
+      nome: [{ value: null, disabled: false }, Validators.required],
+      logradouro: [{ value: null, disabled: false }, Validators.required],
+      numero: [{ value: null, disabled: false, }],
+      complemento: [{ value: null, disabled: false }],
+      bairro: [{ value: null, disabled: false }],
+      cep: [{ value: null, disabled: false }],
+      cidade: [{ value: null, disabled: false }, Validators.required],
+      estado: [{ value: null, disabled: false }, Validators.required],
+      pais: [{ value: null, disabled: false }, Validators.required],
+      nif: [{ value: null, disabled: false }],
+      codigointerno: [{ value: null, disabled: false }]
+    });
+   }
 
 
-  openModal(template: TemplateRef<any>, user?: any) {
+  get formControl() {
+    return this.form.controls;
+  }
+
+  openModal(template: TemplateRef<any>, type?: string, row?) {
+
+    this.form.reset();
+
+    if (type == "update") {
+      this.titleModal = "Atualizar";
+      this.setValueModalUpdate(row);
+
+    } else { this.titleModal = "Novo" }
+
     this.modalRef = this.modalService.show(template, Object.assign({}, { class: 'gray modal-lg' }),);
-
   }
 
   ngOnInit(): void {
-    this.dataTable = {
-      headerRow: ['ID', 'CNPJ', 'Razão', 'Cidade', 'Estado', 'País', 'Ações'],
-      dataRows: []
-    };
+    // this.dataTable = {
+    //   headerRow: ['ID', 'CNPJ', 'Razão', 'Cidade', 'Estado', 'País', 'Ações'],
+    //   dataRows: []
+    // };
 
     this.getAllFornecedor();
     this.getAllPaises();
   }
 
-  loadEvenstDataTable() {
-    this.table = $('#datatable').DataTable({
-      language:
-      {
-        "url": "assets/i18n/Portuguese-Brasil.json"
+  onDelete(row) {
+    Swal.fire({
+
+      title: `Tem certeza?`,
+      text: `Você não poderá reverter isso! (${row.nomeFantasia})`,
+      icon: 'warning',
+      showCancelButton: true,
+      customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger',
+      },
+      confirmButtonText: 'Sim, Delete!',
+      buttonsStyling: false
+
+    }).then((result) => {
+
+      if (result.value) {
+
+        this.service.delete(row.id).subscribe({
+          next: (obj) => {
+
+          },
+          error: (e) => {
+            this.notifyService.showNotification('top', 'right', e.error.message, 'danger');
+            this.isLoadSave = false;
+          },
+          complete: () => {
+            this.getAllFornecedor();
+            Swal.fire(
+              {
+                title: 'Deletado!',
+                text: 'Seu Fornecedor foi excluído.',
+                icon: 'success',
+                customClass: {
+                  confirmButton: "btn btn-success",
+                },
+                buttonsStyling: false
+              }
+            )
+          }
+        })
+
       }
-    });
+    })
+
   }
 
   getAllFornecedor() {
+
     this.isLoad = true;
-    this.subscription = this.service.getAll().subscribe(
-      {
-        next: (obj) => {
 
-          (Object.keys(obj) as (keyof typeof obj)[]).forEach((key, index) => {
-            this.dataTable.dataRows.push(
-              [
-                obj[key]['id'].toString(),
-                obj[key]['cnpj'],
-                obj[key]['razaoSocial'],
-                obj[key]['cidade'],
-                obj[key]['estado'],
-                obj[key]['pais']['nome']
-              ]
-            )
-          });
-
-        },
-        error: (e) => {
-          console.log(e)
-        },
-        complete: () => {
-          // this.isLoad = false;
-          setTimeout(() => {
-            this.isLoad = false;
-            this.loadEvenstDataTable()
-          }, 0);
-        }
-
+    this.service.getAll().subscribe({
+      next: (value) => {
+        this.fornecedorList = value;
+        this.listFornecedorListDynamic = this.fornecedorList
+      }, error: (e) => {
+        this.notifyService.showNotification('top', 'right', e.error.message, 'danger');
+        this.isLoad = false;
+      },
+      complete: () => {
+        this.isLoad = false;
       }
-    )
+    })
+  }
+
+  setValueModalUpdate(row: IFornecedor) {
+
+    this.idUpdate = row.id
+
+    this.formControl.nome.setValue(row.nomeFantasia);
+    this.formControl.logradouro.setValue(row.logradouro);
+    this.formControl.numero.setValue(row.numero);
+    this.formControl.complemento.setValue(row.complemento);
+    this.formControl.bairro.setValue(row.bairro);
+    this.formControl.cep.setValue(row.cep);
+    this.formControl.cidade.setValue(row.cidade);
+    this.formControl.estado.setValue(row.estado);
+    this.formControl.pais.setValue(row.idPais);
+    this.formControl.nif.setValue(row.nif);
+    this.formControl.codigointerno.setValue(row.codigoInterno);
+
   }
 
   getAllPaises() {
@@ -110,4 +189,5 @@ export class FornecedoresComponent implements OnInit {
       }
     )
   }
+
 }
